be8a67707c83656537c97eae50de8f7d12f821ea
Valeria Gora <valeriia308@gmail.com>
2019-04-01 16:05:45 +0300

    Updates

---
+++ precache-manifest.ea9289a8299aa11a7b20113ae02837f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "1452e0696de997b65f6b",
+    "url": "/portfolio/static/js/main.e5e37490.chunk.js"
+  },
+  {
+    "revision": "70eb2ee96321bb30b904",
+    "url": "/portfolio/static/js/2.8805f458.chunk.js"
+  },
+  {
+    "revision": "1452e0696de997b65f6b",
+    "url": "/portfolio/static/css/main.b9282377.chunk.css"
+  },
+  {
+    "revision": "d49ebc85e8a6e26c78cf302fca5dd2ad",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.336f5628eb7cb5148f079b871c162be8.js"
+  "/portfolio/precache-manifest.ea9289a8299aa11a7b20113ae02837f8.js"
 );
 
 workbox.clientsClaim();
