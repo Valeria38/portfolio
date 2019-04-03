98342c2ee501aa4e96d6ed46aa21e5f990a41e93
Valeria Gora <valeriia308@gmail.com>
2019-04-03 17:49:42 +0300

    Updates

--- precache-manifest.1cb4a3d0a2632e663151cea8bbc59937.js
+++ precache-manifest.1cb4a3d0a2632e663151cea8bbc59937.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
   },
   {
-    "revision": "253430b84539b50834ab",
-    "url": "/portfolio/static/js/main.eaea152f.chunk.js"
+    "revision": "f5cfd6218d2f0c3b621c",
+    "url": "/portfolio/static/js/main.3d13b843.chunk.js"
   },
   {
     "revision": "f923e061e3aa1c9c0246",
     "url": "/portfolio/static/js/2.4ea0a39f.chunk.js"
   },
   {
-    "revision": "253430b84539b50834ab",
-    "url": "/portfolio/static/css/main.f648b572.chunk.css"
+    "revision": "f5cfd6218d2f0c3b621c",
+    "url": "/portfolio/static/css/main.b401135a.chunk.css"
   },
   {
     "revision": "f923e061e3aa1c9c0246",
     "url": "/portfolio/static/css/2.810aa397.chunk.css"
   },
   {
-    "revision": "4bd7862daa7cd19cb0f4393f824f075d",
+    "revision": "6f123025c4a26020c9b35b38a9a25eed",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f351781b717074da98266b32db0aee6c.js"
+  "/portfolio/precache-manifest.1cb4a3d0a2632e663151cea8bbc59937.js"
 );
 
 workbox.clientsClaim();
