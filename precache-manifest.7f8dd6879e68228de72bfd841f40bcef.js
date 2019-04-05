e624b439e36a7a7081b5527bd0688b99a92989c8
Valeria Gora <valeriia308@gmail.com>
2019-04-06 00:02:20 +0300

    Updates

---
+++ precache-manifest.7f8dd6879e68228de72bfd841f40bcef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "57f974f95cfd8c04ba6a",
+    "url": "/portfolio/static/js/main.7d41f029.chunk.js"
+  },
+  {
+    "revision": "ab0550baac7c1ce1601b",
+    "url": "/portfolio/static/js/2.582b7511.chunk.js"
+  },
+  {
+    "revision": "57f974f95cfd8c04ba6a",
+    "url": "/portfolio/static/css/main.31a9868e.chunk.css"
+  },
+  {
+    "revision": "ab0550baac7c1ce1601b",
+    "url": "/portfolio/static/css/2.810aa397.chunk.css"
+  },
+  {
+    "revision": "776e92fa5f68b9c878c05c195b5facb5",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.190e79a9a84da1afa25b448314471ce7.js"
+  "/portfolio/precache-manifest.7f8dd6879e68228de72bfd841f40bcef.js"
 );
 
 workbox.clientsClaim();
